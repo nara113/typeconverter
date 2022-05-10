69e362c3ad8194760eebac80e496200586fb0917
nara113 <0554my@naver.com>
2022-05-11 05:39:48 +0900

    스프링 타입 컨버터 - 포맷터 - Formatter

---
+++ src/main/java/hello/typeconverter/formatter/MyNumberFormatter.java
@@ -0,0 +1,19 @@
+package hello.typeconverter.formatter;
+
+import org.springframework.format.Formatter;
+
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.Locale;
+
+public class MyNumberFormatter implements Formatter<Number> {
+    @Override
+    public Number parse(String text, Locale locale) throws ParseException {
+        return NumberFormat.getInstance(locale).parse(text);
+    }
+
+    @Override
+    public String print(Number object, Locale locale) {
+        return NumberFormat.getInstance(locale).format(object);
+    }
+}
