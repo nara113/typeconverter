8375992e2ee609ae69ec7d6a5e4a31828db7b683
nara113 <0554my@naver.com>
2022-05-11 04:29:33 +0900

    스프링 타입 컨버터 - 스프링에 Converter 적용하기

--- src/main/java/hello/typeconverter/controller/HelloController.java
+++ src/main/java/hello/typeconverter/controller/HelloController.java
@@ -1,8 +1,19 @@
 package hello.typeconverter.controller;
 
+import hello.typeconverter.type.IpPort;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    @GetMapping("/hello-v2")
+    public Integer convert(@RequestParam Integer data) {
+        return data;
+    }
 
+    @GetMapping("/hello-v3")
+    public IpPort convertIpPort(@RequestParam IpPort data) {
+        return data;
+    }
 }
